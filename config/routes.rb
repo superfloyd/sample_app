3741105091f1b1229fb4b42756e40325210e7343
superfloyd <superfloyd@gmail.com>
2013-05-22 12:37:14 -0700

    again routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
  
    root to: 'pages#home'
 
-  match '/home',    to: 'pages#home'
+  #match '/home',    to: 'pages#home'
   match '/help',    to: 'pages#help'
   match '/about',   to: 'pages#about'
   match '/contact', to: 'pages#contact'
